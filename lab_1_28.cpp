8a9d5f6ce3613f13e3c948c92f54aadd23c13875
Br0dy64 <77024856+Brodyga63@users.noreply.github.com>
2024-10-12 23:55:45 +0400

    Update lab_1_28.cpp

--- lab_1_28.cpp
+++ lab_1_28.cpp
@@ -1,28 +1,64 @@
-/*
-  Родионов Антон 6114-100503D
-  Лабораторная работа #1
-     Вариант 28
-*/
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
+
 #define m 50
 #define n 50
+#define k 100
 
-
-int chr[m][n] = { 0 };
-int width, height;
-
+int vector_A[m] = { 0 }, vector_B[n] = { 0 }, L, LAB, result[k];
 
 int main() {
-  setlocale(LC_ALL, "RU");
-  printf("Введите количество строк: ");
-  scanf("%d \n", &width);
-  printf("Введите количество столбцов: ");
-  scanf("%d\n", &height);
+    setlocale(LC_ALL, "RU");
+    printf("Введите количество элементов в векторах A и B: ");
+    scanf("%d", &L);
 
-  printf("%d %d", width, height);
+    LAB = L + L;
 
+    printf("\nВведите элементы вектора A:\n");
+    for (int i = 0; i < L; i++) {
+        printf("Введите элемент [%d]: ", i + 1);
+        scanf("%d", &vector_A[i]);
+    }
+    printf("\nВведите элементы вектора B:\n");
+    for (int i = 0; i < L; i++) {
+        printf("Введите элемент [%d]: ", i + 1);
+        scanf("%d", &vector_B[i]);
+    }
 
+    printf("\nvector A = { ");
+    for (int i = 0; i < L; i++) {
+        printf("%d ", vector_A[i]);
+    }
+    printf("} \n");
+    printf("vector B = { ");
+    for (int i = 0; i < L; i++) {
+        printf("%d ", vector_B[i]);
+    }
+    printf("} \n\n");
+
+    
+
+    for (int i = 0, o = 0; i < L; i++) {
+        if (vector_A[i] < vector_B[i]) {
+            result[o] = vector_B[i];
+            result[o + 1] = vector_A[i];
+            o += 2;
+        }
+        else {
+            result[o] = vector_A[i];
+            result[o + 1] = vector_B[i];
+            o += 2;
+        }
+    }
+
+    
+
+    // Печать результата
+    printf("result = { ");
+    for (int i = 0; i < LAB; i++) {
+        printf("%d ", result[i]);
+    }
+    printf("}");
+    return 0;
 }
