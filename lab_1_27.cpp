4f0d9167658c702a6a777fcc407d76f9b250e4bc
Br0dy64 <77024856+Brodyga63@users.noreply.github.com>
2024-10-16 18:01:37 +0400

    Update lab_1_27.cpp

--- lab_1_27.cpp
+++ lab_1_27.cpp
@@ -1,63 +1,79 @@
 /*
   Родионов Антон 6114-100503D
-  Лабораторная работа #1
-     Вариант 27
+    Лабораторная работа #1
+         Вариант 27
 */
+
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
-#define m 50
+#define m 100
 #define n 50
 #define k 50
 
 
 int chr[m][n] = { 0 }, result[k];
-int width, height, aboba;
+int width, height, aboba, abobam;
 
 
 int main() {
     setlocale(LC_ALL, "RU");
-    printf("Введите количество строк: ");
-    scanf("%d", &height);
-    printf("Введите количество столбцов: ");
-    scanf("%d", &width);
-
-    printf("строк = %d, столбцов = %d \n\n", height, width);
-
-    // Ввод элементов матрицы
-    printf("Введите элементы матрицы A:\n");
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            printf("Элемент [%d][%d]: ", i + 1, j + 1);
-            scanf_s("%d", &chr[i][j]);
+    while (true) {
+        printf("Введите количество строк: ");
+        scanf("%d", &height); 
+        if (height > 0 && height <= m) { 
+            break; 
         }
+       
     }
-    printf("\n");
-    // Вывод матрицы в двухмерном виде
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            printf("%4d", chr[i][j]);
+    while (true) {
+        printf("Введите количество столбцов: ");
+        scanf("%d", &width); 
+        if (width > 0 && width <= n) { 
+            break;
         }
-        printf("\n");
-    }
-    for (int i = 0; i < height; i++) {
-        int max = -1000;
-        for (int j = 0; j < width; j++) {
-            if (chr[i][j] > max) {
-                max = chr[i][j];
-                aboba = j;
+        
+        }
+
+        printf("строк = %d, столбцов = %d \n\n", height, width);
+
+        // Ввод элементов матрицы
+        printf("Введите элементы матрицы A:\n");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                printf("Элемент A[%d][%d]: ", i + 1, j + 1);
+                scanf_s("%d", &chr[i][j]);
             }
         }
-        result[i] = aboba+1;
-    }
+        printf("\n");
+        // Вывод матрицы в двухмерном виде
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                printf("%4d", chr[i][j]);
+            }
+            printf("\n");
+        }
+        // Обработка матрицы
+        for (int i = 0; i < height; i++) {
+            int max = -10000;
+            int min = 10000;
+            for (int j = 0; j < width; j++) {
+                if (chr[i][j] > max) {
+                    max = chr[i][j];
+                    aboba = chr[i][j];
+                }
+                if (chr[i][j] < min) {
+                    min = chr[i][j];
+                    abobam = chr[i][j];
+                }
+            }
+            result[i] = aboba + abobam;
+        }
 
-    
-
-    printf("\nresult = {");
-    for (int i = 0; i < height; i++) {
-        printf(" %d", result[i]);
-        ;
+        // Вывод результата
+        printf("\nresult = ");
+        for (int i = 0; i < height; i++) {
+            printf("%d ", result[i]);
+        }
     }
-    printf(" }");
-}
